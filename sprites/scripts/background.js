2005387a2d29f66196d8f7b08991b5ebe5f62c4e
Robin Singh <vougishtiger@gmail.com>
2024-08-29 19:20:00 -0700

    created energy bar, energy handle, and charge functions

--- sprites/scripts/background.js
+++ sprites/scripts/background.js
@@ -14,9 +14,9 @@ class Background {
   }
   draw(){
     this.game.ctx.drawImage(this.image, this.x, 0, this.scaleWidth, this.scaleHeight);
-    this.game.ctx.drawImage(this.image, this.x + this.scaleWidth- 1, 0, this.scaleWidth, this.scaleHeight);
+    this.game.ctx.drawImage(this.image, this.x + this.scaleWidth- 2, 0, this.scaleWidth, this.scaleHeight);
     if(this.game.canvas.width >= this.scaleWidth){
-      this.game.ctx.drawImage(this.image, this.x + this.scaleWidth*2-2, 0, this.scaleWidth, this.scaleHeight);
+      this.game.ctx.drawImage(this.image, this.x + this.scaleWidth*2-4, 0, this.scaleWidth, this.scaleHeight);
     }
   }
   resize(){
